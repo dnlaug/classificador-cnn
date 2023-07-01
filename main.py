17916d60cbe25b277ffa9a173753f90a5d618f46
Daniel A. Augusto <daniamaug@gmail.com>
2023-07-01 00:57:53 -0300

    Criação da main.py
    Criação das funções e main()

---
+++ main.py
@@ -0,0 +1,29 @@
+# Função personalizada para carregar o dataset CIFAR-10 com barra de progresso
+def carrega_dataset():
+    return
+
+# Função para pré-processar os dados
+def preprocess_dados():
+    return
+
+# Função para definir o modelo CNN
+def define_modelo():
+    return
+
+# Função para treinar o modelo
+def treina_modelo():
+    return
+
+# Função para avaliar o modelo
+def avalia_modelo():
+    return
+
+# Função para plotar a matriz de confusão
+def matriz_conf():
+    return
+
+# Função main
+def main():
+
+# if __name__ == '__main__':
+    main()
